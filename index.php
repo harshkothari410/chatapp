863e1770b369abd6604153df46ebc9177e4ddec8
harshkothari410 <harshkothari410@gmail.com>
2013-03-27 21:19:53 +0530

    Added files of project

---
+++ index.php
@@ -0,0 +1,133 @@
+<?
+session_start();
+
+if(isset($_GET['logout'])){	
+	
+	//Simple exit message
+	$fp = fopen("log.html", 'a');
+	fwrite($fp, "<div class='msgln'><i>User ". $_SESSION['name'] ." has left the chat session.</i><br></div>");
+	fclose($fp);
+	
+	session_destroy();
+	header("Location: index.php"); //Redirect the user
+}
+
+function loginForm(){
+	echo'
+	<div id="loginform">
+	<form action="index.php" method="post">
+		<p>Please enter your name to continue:</p>
+		<label for="name">Name:</label>
+		<input type="text" name="name" id="name" />
+		<input type="submit" name="enter" id="enter" value="Enter" />
+	</form>
+	</div>
+	';
+}
+
+if(isset($_POST['enter'])){
+	if($_POST['name'] != ""){
+		$_SESSION['name'] = stripslashes(htmlspecialchars($_POST['name']));
+	}
+	else{
+		echo '<span class="error">Please type in a name</span>';
+	}
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Chat Application</title>
+	<link type="text/css" rel="stylesheet" href="style.css" />
+</head>
+<body>
+	
+<h1 id="heading1">Simple Chat Application</h1>
+<?php
+if(!isset($_SESSION['name'])){
+	loginForm();
+}
+else{
+?>
+<div id="wrapper">
+	<div id="menu">
+		<p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
+		<p class="logout"><a id="exit" href="#">Exit Chat</a></p>
+		<div style="clear:both"></div>
+	</div>	
+	<div id="chatbox"><?php
+	if(file_exists("log.html") && filesize("log.html") > 0){
+		$handle = fopen("log.html", "r");
+		$contents = fread($handle, filesize("log.html"));
+		fclose($handle);
+		
+		echo $contents;
+	}
+	?></div>
+	
+	<form name="message" action="">
+		<input name="usermsg" type="text" id="usermsg" size="63" />
+		<input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
+	</form>
+</div>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+<script type="text/javascript">
+// jQuery Document
+$(document).ready(function(){
+	//If user submits the form
+	$("#submitmsg").click(function(){	
+		var clientmsg = $("#usermsg").val();
+		$.post("post.php", {text: clientmsg});				
+		$("#usermsg").attr("value", "");
+		return false;
+	});
+	
+	//Load the file containing the chat log
+	function loadLog(){		
+		var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20;
+		$.ajax({
+			url: "log.html",
+			cache: false,
+			success: function(html){		
+				$("#chatbox").html(html); //Insert chat log into the #chatbox div				
+				var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20;
+				if(newscrollHeight > oldscrollHeight){
+					$("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+				}				
+		  	},
+		});
+	}
+	setInterval (loadLog, 2500);	//Reload file every 2.5 seconds
+	
+	//If user wants to end session
+	$("#exit").click(function(){
+		var exit = confirm("Are you sure you want to end the session?");
+		if(exit==true){window.location = 'index.php?logout=true';}		
+	});
+});
+</script>
+<?php
+}
+?>
+
+
+
+
+	<div class="footer">
+		<div>
+			
+
+			
+			<div class="connect">
+				<h2 >Connect with me</h2>
+				<a href="http://facebook.com/harshkothari410" class="facebook">Facebook</a> 
+				<a href="http://twitter.com/harshkothari410" class="twitter">Twitter</a> 
+				<a href="https://plus.google.com/u/0/103880490704189416632" class="google">Google+</a>
+			</div>
+		</div>
+		
+	</div>
+</body>
+</html>
